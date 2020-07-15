39a3c21f3f1c289a3090ad6a72a5b06a09cb8b98
yuxh00 <yuxh00@gmail.com>
2020-07-15 23:31:01 +0800

    type class example

---
+++ src/main/scala/typeclasses/v1_humanlike/HumanLikeDriver.scala
@@ -0,0 +1,28 @@
+package typeclasses.v1_humanlike
+
+sealed trait Animal
+final case class Dog(name: String)  extends Animal
+final case class Cat(name: String)  extends Animal
+final case class Bird(name: String) extends Animal
+
+object HumanLikeDriver extends App {
+
+  import BehavesLikeHumanInstances.dogBehavesLikeHuman
+
+  val rover = Dog("Rover")
+  val mimi  = Cat("Mimi")
+  // (3a) apply the functions to the Dog instance
+  //import BehavesLikeHuman.{speak, eatHumanFood}  //can also import the functions
+  BehavesLikeHuman.speak(rover)(dogBehavesLikeHuman)
+  BehavesLikeHuman.eatHumanFood(rover)(dogBehavesLikeHuman)
+
+  // (3b) import the function, and you can call it on the Dog instance
+  import BehavesLikeHumanSyntax.BehavesLikeHumanOps
+  rover.speak
+  rover.eatHumanFood
+  mimi.speak
+  // the function isn't implemented for a Cat or Bird, so these won't work
+  //speak(Cat("Garfield"))
+  //speak(Bird("Polly"))
+
+}
